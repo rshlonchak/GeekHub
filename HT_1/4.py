bd3cf508ce5d568ce4c6464305271a7b803c2fb8
Roman <rshlonchak@gmail.com>
2021-11-18 13:19:41 +0200

    update 4

--- HT_1/4.py
+++ HT_1/4.py
@@ -6,6 +6,6 @@
 number = int(input('Please enter a number:'))
 sum_str = ""
 for i in range(number):
-    my_str = input('Please enter items through the space:')
+    my_str = input('Please enter your strings:')
     sum_str += my_str
 print(sum_str)
