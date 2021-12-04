835fe498fdb58913a4aec064633ca3a323bf66f1
Roman <rshlonchak@gmail.com>
2021-12-04 17:58:59 +0200

    update 6

--- HT_5/6.py
+++ HT_5/6.py
@@ -6,19 +6,30 @@
 """
 
 
-def range2(min_value, max_value, step=1):
-    value = min_value
-    if step > 0:
-        while value < max_value:
-            yield value
-            value += step
+def range2(min_value, max_value=0, step=1):
+    if max_value != 0:
+        value = min_value
+        if step > 0:
+            while value < max_value:
+                yield value
+                value += step
+        else:
+            while value > max_value:
+                yield value
+                value += step
     else:
-        while value > max_value:
-            yield value
-            value += step
+        value_1 = min_value
+        min_value = max_value
+        max_value = value_1
+        value = min_value
+        if step > 0:
+            while value < max_value:
+                yield value
+                value += step
 
 
 print(range2(0, 20))
+print(list(range2(20)))
 print(list(range2(0, 20)))
 print(list(range2(0, 20, 2)))
 print(list(range2(20, 1, -2)))
